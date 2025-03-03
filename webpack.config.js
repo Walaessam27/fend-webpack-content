94da55041450eb891cf195d801d091de59aa2fdc
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2025-03-03 14:09:33 +0200

    step 4 plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Added CleanWebpackPlugin
 
 module.exports = {
-    entry: './src/client/index.js',
+
+    mode: "development",
+ entry: './src/client/index.js',
     devtool: 'source-map',
+    output: { // Added output configuration
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
     module: {
         rules: [
             {
@@ -15,9 +22,10 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(), // Cleans dist folder before each build
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
         })
     ]
-}
+};
